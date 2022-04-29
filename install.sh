9d97744b57977792d43537aae965f14546d4a7aa
Worker3 <hoonnhun711@gmail.com>
2022-04-29 17:16:53 -0400

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
-wget -o LM_2.3.1.tgz https://www.dropbox.com/s/zrnx1x2284zwszl/Leap_Motion_SDK_Linux_2.3.1.tgz?dl=0
-tar zxf LM_2.3.1.tgz
+#echo Downloading SDK...
+#wget https://www.dropbox.com/s/zrnx1x2284zwszl/Leap_Motion_SDK_Linux_2.3.1.tgz
+echo Unzip file..
+#tar zxf Leap_Motion_SDK_Linux_2.3.1.tgz
+
+echo Installing Deb
+sudo apt-get install libgl1-mesa-* libxi6 libdbus-1-3 libxxf86vm1 libc6 -y
+sudo apt-get install -f ./LeapDeveloperKit_2.3.1+31549_linux/Leap-2.3.1+31549-x64.deb
